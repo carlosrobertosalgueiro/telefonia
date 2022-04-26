ce0ba88c5291772c4bb5f87aedb64f86abac7f76
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-04-26 00:23:05 -0300

    insered module assinet subscriber

--- lib/assinante.ex
+++ lib/assinante.ex
@@ -1,13 +1,19 @@
 defmodule Assinante do
   defstruct nome: nil, numero: nil, cpf: nil
     def cadastrar(nome, numero, cpf) do
-        %__MODULE__{nome: nome, numero: numero, cpf: cpf}
+       read() ++  [%__MODULE__{nome: nome, numero: numero, cpf: cpf}]
         |> :erlang.term_to_binary()
         |> write()
-
     end
 
     defp write(lista_assinantes) do
-        File.write("Assinantes.txt", lista_assinantes)
+        File.write("assinantes.txt", lista_assinantes)
     end
+
+    def read() do
+     {:ok, assinantes} = File.read("assinantes.txt")
+     assinantes
+     |> :erlang.binary_to_term()
+    end
+
 end
