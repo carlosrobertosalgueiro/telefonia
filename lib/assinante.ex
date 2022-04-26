d07ce3c441cf37a2dbfa6fa84a3900d642415cf4
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-04-26 00:56:48 -0300

    leval 1 completed

--- lib/assinante.ex
+++ lib/assinante.ex
@@ -1,17 +1,19 @@
 defmodule Assinante do
-  defstruct nome: nil, numero: nil, cpf: nil
-    def cadastrar(nome, numero, cpf) do
-       read() ++  [%__MODULE__{nome: nome, numero: numero, cpf: cpf}]
+  defstruct nome: nil, numero: nil, cpf: nil, plano: nil
+
+ @assinantes %{:prepago => "pre.txt", :pospago => "pos.txt"}
+    def cadastrar(nome, numero, cpf, plano \\ :prepago) do
+       read(plano) ++  [%__MODULE__{nome: nome, numero: numero, cpf: cpf, plano: plano}]
         |> :erlang.term_to_binary()
-        |> write()
+        |> write(plano)
     end
 
-    defp write(lista_assinantes) do
-        File.write("assinantes.txt", lista_assinantes)
+    defp write(lista_assinantes, plano) do
+        File.write(@assinantes[plano], lista_assinantes)
     end
 
-    def read() do
-     {:ok, assinantes} = File.read("assinantes.txt")
+    def read(plano) do
+     {:ok, assinantes} = File.read(@assinantes[plano])
      assinantes
      |> :erlang.binary_to_term()
     end
