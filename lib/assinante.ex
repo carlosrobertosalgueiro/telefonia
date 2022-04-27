90641fa4fa46bbdd2709a8cd16970a1f4938c0fb
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-04-27 11:32:39 -0300

    insertedo fiiter function

--- lib/assinante.ex
+++ lib/assinante.ex
@@ -3,35 +3,14 @@ defmodule Assinante do
 
   @assinantes %{:prepago => "pre.txt", :pospago => "pos.txt"}
 
-    def buscar_assinantes(numero, key \\ :all) do
-        buscar(numero, key)
-    end
-
-    defp buscar(numero, :prepago) do
-        IO.inspect("=== Assinantes Prepago ===")
-
-        assinantes_prepago()
-        |> Enum.find(assinantes(), &(&1.numero == numero))
-    end
-
-    defp buscar(numero, :pospago) do
-        IO.inspect("=== Assinantes Pospago ===")
-
-        assinantes_pospago()
-        |> Enum.find(assinantes(), &(&1.numero == numero))
-    end
-
-    defp buscar(numero, :all) do
-        IO.inspect("=== Todos os Assinantes ===")
-
-        assinantes()
-        |> Enum.find(assinantes(), &(&1.numero == numero))
-    end
+    def buscar_assinantes(numero, key \\ :all), do: buscar(numero, key)
+    defp buscar(numero, :prepago), do: filtro(assinantes_prepago(), numero)
+    defp buscar(numero, :pospago), do: filtro(assinantes_pospago(), numero)
+    defp buscar(numero, :all), do: filtro(assinantes(), numero)
+    defp filtro(lista, numero), do: Enum.find(lista, &(&1.numero == numero))
 
     def assinantes, do: read(:prepago) ++ read(:pospago)
-
     def assinantes_prepago, do: read(:prepago)
-
     def assinantes_pospago, do: read(:pospago)
 
     def cadastrar(nome, numero, cpf, plano \\ :prepago) do
@@ -48,14 +27,16 @@ defmodule Assinante do
         end
     end
 
-    defp write(lista_assinantes, plano) do
-        File.write(@assinantes[plano], lista_assinantes)
-    end
+    defp write(lista_assinantes, plano), do: File.write(@assinantes[plano], lista_assinantes)
 
     def read(plano) do
-        {:ok, assinantes} = File.read(@assinantes[plano])
+        case File.read(@assinantes[plano]) do
+            {:ok, assinantes} ->
+               assinantes
+               |> :erlang.binary_to_term()
 
-        assinantes
-        |> :erlang.binary_to_term()
+            {:error, :ennoent} ->
+               {:error, "arquivo invalido"}
+        end
     end
 end
