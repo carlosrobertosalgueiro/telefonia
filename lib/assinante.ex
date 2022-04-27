67333013e31bc4367bc19ee5a7ed56340dce1485
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-04-27 02:14:45 -0300

    inserted function buscar

--- lib/assinante.ex
+++ lib/assinante.ex
@@ -3,33 +3,59 @@ defmodule Assinante do
 
   @assinantes %{:prepago => "pre.txt", :pospago => "pos.txt"}
 
-  def buscar_assinantes(numero) do
-    (read(:prepago) ++ read(:pospago))
-    |> Enum.find(fn assinante -> assinante.numero == numero end)
-  end
-
-  def cadastrar(nome, numero, cpf, plano \\ :prepago) do
-    case buscar_assinantes(numero) do
-      nil ->
-        (read(plano) ++ [%__MODULE__{nome: nome, numero: numero, cpf: cpf, plano: plano}])
-        |> :erlang.term_to_binary()
-        |> write(plano)
-
-        {:ok, "Assinante #{nome} cadastrado com sucesso!"}
-
-      _assinate ->
-        {:error, "Assinante #{nome} já cadastrado"}
+    def buscar_assinantes(numero, key \\ :all) do
+        buscar(numero, key)
     end
-  end
 
-  defp write(lista_assinantes, plano) do
-    File.write(@assinantes[plano], lista_assinantes)
-  end
+    defp buscar(numero, :prepago) do
+        IO.inspect("=== Assinantes Prepago ===")
 
-  def read(plano) do
-    {:ok, assinantes} = File.read(@assinantes[plano])
+        assinantes_prepago()
+        |> Enum.find(assinantes(), &(&1.numero == numero))
+    end
 
-    assinantes
-    |> :erlang.binary_to_term()
-  end
+    defp buscar(numero, :pospago) do
+        IO.inspect("=== Assinantes Pospago ===")
+
+        assinantes_pospago()
+        |> Enum.find(assinantes(), &(&1.numero == numero))
+    end
+
+    defp buscar(numero, :all) do
+        IO.inspect("=== Todos os Assinantes ===")
+
+        assinantes()
+        |> Enum.find(assinantes(), &(&1.numero == numero))
+    end
+
+    def assinantes, do: read(:prepago) ++ read(:pospago)
+
+    def assinantes_prepago, do: read(:prepago)
+
+    def assinantes_pospago, do: read(:pospago)
+
+    def cadastrar(nome, numero, cpf, plano \\ :prepago) do
+        case buscar_assinantes(numero) do
+        nil ->
+            (read(plano) ++ [%__MODULE__{nome: nome, numero: numero, cpf: cpf, plano: plano}])
+            |> :erlang.term_to_binary()
+            |> write(plano)
+
+            {:ok, "Assinante #{nome} cadastrado com sucesso!"}
+
+        _assinate ->
+            {:error, "Assinante #{nome} já cadastrado"}
+        end
+    end
+
+    defp write(lista_assinantes, plano) do
+        File.write(@assinantes[plano], lista_assinantes)
+    end
+
+    def read(plano) do
+        {:ok, assinantes} = File.read(@assinantes[plano])
+
+        assinantes
+        |> :erlang.binary_to_term()
+    end
 end
