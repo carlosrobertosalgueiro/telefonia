1859d75a7862b7122a9c1b5358bd73281291992e
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-05-02 23:43:00 -0300

    refactored

--- lib/assinante.ex
+++ lib/assinante.ex
@@ -5,7 +5,7 @@ defmodule Assinante do
   A mais ultilizada é a função `cadastrar/4`
   """
 
-  defstruct nome: nil, numero: nil, cpf: nil, plano: nil
+  defstruct nome: nil, numero: nil, cpf: nil, plano: nil, chamadas: []
 
   @assinantes %{:prepago => "pre.txt", :pospago => "pos.txt"}
 
@@ -41,13 +41,15 @@ defmodule Assinante do
   """
   def cadastrar(nome, numero, cpf, :prepago), do: cadastrar(nome, numero, cpf, %Prepago{})
   def cadastrar(nome, numero, cpf, :pospago), do: cadastrar(nome, numero, cpf, %Pospago{})
+
   def cadastrar(nome, numero, cpf, plano) do
     case buscar_assinantes(numero) do
       nil ->
         assinante = %__MODULE__{nome: nome, numero: numero, cpf: cpf, plano: plano}
-        (read(pagar_plano(assinante)) ++ [assinante])
+
+        (read(pegar_plano(assinante)) ++ [assinante])
         |> :erlang.term_to_binary()
-        |> write(pagar_plano(assinante))
+        |> write(pegar_plano(assinante))
 
         {:ok, "Assinante #{nome} cadastrado com sucesso!"}
 
@@ -56,10 +58,21 @@ defmodule Assinante do
     end
   end
 
-  defp write(lista_assinantes, plano), do: File.write(@assinantes[plano], lista_assinantes)
+  def atualizar(numero, assinante) do
+    {assinante_antigo, nova_lista} = deletar_item(numero)
 
+    case assinante.plano.__struct__ == assinante_antigo.plano.__struct__ do
+      true ->
+        (nova_lista ++ [assinante])
+        |> :erlang.term_to_binary()
+        |> write(pegar_plano(assinante))
 
-  defp pagar_plano(assinante) do
+      false ->
+        {:error, "Assinante não pore alterar seu plano"}
+    end
+  end
+
+  defp pegar_plano(assinante) do
     case assinante.plano.__struct__ == Prepago do
       true -> :prepago
       false -> :pospago
@@ -67,16 +80,26 @@ defmodule Assinante do
   end
 
   def deletar(numero) do
+    {assinante, nova_lista} = deletar_item(numero)
+
+    nova_lista
+    |> :erlang.term_to_binary()
+    |> write(assinante.plano)
+
+    {:ok, "Assinante #{assinante.nome} deletado!"}
+  end
+
+  def deletar_item(numero) do
     assinante = buscar_assinantes(numero)
 
-    result_delete =
-      assinantes()
+    nova_lista =
+      read(pegar_plano(assinante))
       |> List.delete(assinante)
-      |> :erlang.term_to_binary()
-      |> write(pagar_plano(assinante))
 
-    {result_delete, "Assinante #{assinante.nome} deletado!"}
+    {assinante, nova_lista}
   end
+
+  defp write(lista_assinantes, plano), do: File.write!(@assinantes[plano], lista_assinantes)
 
   def read(plano) do
     case File.read(@assinantes[plano]) do
