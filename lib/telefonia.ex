383a4f9171b5ae19ed3facfa4ca503e505506973
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-05-04 01:37:09 -0300

    finished

--- lib/telefonia.ex
+++ lib/telefonia.ex
@@ -4,7 +4,50 @@ defmodule Telefonia do
     File.write("pos.txt", :erlang.term_to_binary([]))
   end
 
-  def cadastrar_assinate(nome, numero, cpf, plano) do
+  def cadastrar_assinante(nome, numero, cpf, plano) do
     Assinante.cadastrar(nome, numero, cpf, plano)
+  end
+
+  def listar_assinantes, do: Assinante.assinantes()
+  def listar_assinantes_prepago, do: Assinante.assinantes_prepago()
+  def listar_assinantes_pospago, do: Assinante.assinantes_pospago()
+
+  def fazer_chamada(numero, plano, data, duracao) do
+    cond do
+      plano == :prepago -> Prepago.fazer_chamada(numero, data, duracao)
+      plano == :pospago -> Pospago.fazer_chamada(numero, data, duracao)
+    end
+  end
+
+  def recarga(numero, data, valor), do: Recarga.nova(data, valor, numero)
+
+  def buscar_por_numero(numero, plano \\ :all), do: Assinante.buscar_assinantes(numero, plano)
+
+  def imprimir_contas(mes, ano) do
+    Assinante.assinantes_prepago()
+    |> Enum.each(fn assinante ->
+      assinante = Prepago.imprimir_conta(mes, ano, assinante.numero)
+      IO.puts("====== Conta Prepaga do assinante #{assinante.nome} ======")
+      IO.puts("Número do assinante: #{assinante.numero}")
+      IO.puts("Chamadas:")
+      IO.inspect(assinante.chamadas)
+      IO.puts("Recargas:")
+      IO.inspect(assinante.plano.recargas)
+      IO.puts("Total de chamadas: #{Enum.count(assinante.chamadas)}")
+      IO.puts("Total de recargas: #{Enum.count(assinante.plano.recargas)}")
+      IO.puts("===========================================================")
+    end)
+
+    Assinante.assinantes_pospago()
+    |> Enum.each(fn assinante ->
+      assinante = Pospago.imprimir_conta(mes, ano, assinante.numero)
+      IO.puts("======= Conta Pospaga do assinante #{assinante.nome} ======")
+      IO.puts("Número do assinante: #{assinante.numero}")
+      IO.puts("Chamadas:")
+      IO.inspect(assinante.chamadas)
+      IO.puts("Total de chamadas: #{Enum.count(assinante.chamadas)}")
+      IO.puts("Total da fatura: #{assinante.plano.valor}")
+      IO.puts("===========================================================")
+    end)
   end
 end
